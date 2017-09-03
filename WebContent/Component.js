aa1570774220edf4e5d41c16d3701869cca55b5f
devendervb12 <info@s-max.in>
2017-09-03 07:27:57 +0530

    initial Commit

---
+++ WebContent/Component.js
@@ -0,0 +1,10 @@
+// define a component - smax.batch24
+
+sap.ui.core.UIComponent.extend("smax.batch24.Component", {
+	metadata : {},
+	createContent : function(){
+		
+		var oButton = new sap.m.Button({text : "Register"})
+		return oButton;
+	}
+});
